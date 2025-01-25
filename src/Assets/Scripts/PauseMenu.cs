3aeb374ae99a85b9ea785b23c3b54e2d4b0d3514
darken <jan10drp@gmail.com>
2025-01-25 15:22:43 +0100

    blur

--- src/Assets/Scripts/PauseMenu.cs
+++ src/Assets/Scripts/PauseMenu.cs
@@ -4,16 +4,31 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject PauseButton;
     [SerializeField] private GameObject PauseMenuUI;
+    [SerializeField] private GameObject blurBackground;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(Time.timeScale == 1) {
+                pause();
+            }
+            else {
+                resume();
+            }
+        }
+    }
     public void pause() {
         Time.timeScale = 0f; // pausa el juego
         PauseButton.SetActive(false);
         PauseMenuUI.SetActive(true);
+        blurBackground.SetActive(true);
     }
 
     public void resume() {
         Time.timeScale = 1f; // reanuda el juego
         PauseButton.SetActive(true);
         PauseMenuUI.SetActive(false);
+        blurBackground.SetActive(false);
     }
+
 }   
