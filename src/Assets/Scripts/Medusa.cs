29c202685c66f53db6ee5bb9568f257e241f3d20
zVictörium <profreegameyt@gmail.com>
2025-01-25 23:02:41 +0100

    Update medusas

--- src/Assets/Scripts/Medusa.cs
+++ src/Assets/Scripts/Medusa.cs
@@ -4,7 +4,7 @@ public class Medusa : MonoBehaviour
 {
     [SerializeField] private Vector3 positionA;
     [SerializeField] private Vector3 positionB;
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float moveSpeed = 0.75f;
     
     private float lerpTime = 0f;
     private bool movingToB = true;
@@ -15,7 +15,7 @@ public class Medusa : MonoBehaviour
         
         if (movingToB)
         {
-            float smoothValue = Mathf.SmoothStep(0f, 1f, lerpTime);
+            float smoothValue = Mathf.SmoothStep(0f, 1f, Mathf.Pow(lerpTime, 3));
             transform.position = Vector3.Lerp(positionA, positionB, smoothValue);
             if (lerpTime >= 1f)
             {
@@ -25,7 +25,7 @@ public class Medusa : MonoBehaviour
         }
         else
         {
-            float smoothValue = Mathf.SmoothStep(0f, 1f, lerpTime);
+            float smoothValue = Mathf.SmoothStep(0f, 1f, Mathf.Pow(lerpTime, 3));
             transform.position = Vector3.Lerp(positionB, positionA, smoothValue);
             if (lerpTime >= 1f)
             {
